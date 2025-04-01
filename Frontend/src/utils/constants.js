2540e6d0de44787d244432d483a96462abd026a5
Shivam Sharma <sharmashivam6585@gmail.com>
2025-04-01 07:10:04 +0530

    remove frontend  folder from root

--- Frontend/src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const MESSAGES = {
-  INVALID_CREDENTIALS: 'Invalid username or password. Please try again.',
-  TASK_ADDED: 'Task added successfully!',
-  TASK_DELETED: 'Task deleted successfully!',
-  LOGIN_SUCCESS: 'Login successful!',
-  LOGOUT_SUCCESS: 'Logout successful!',
-  TASK_ERROR: 'An error occurred while managing tasks.',
-};
